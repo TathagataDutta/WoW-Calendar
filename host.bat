d40b5c4cc43ff2db1b274579389d8dda55e55018
Tathagata Dutta <tathagata.dk@gmail.com>
2021-05-17 02:58:42 -0400

    uvicorn[standard] fix ;-;

---
+++ host.bat
@@ -0,0 +1,2 @@
+cd "D:\Python\WoW-Calendar"
+gcloud app deploy --version 1 --project wc-be-test --quiet
--- main.py
+++ main.py
@@ -176,5 +176,5 @@ async def fetch_raids(user_id: str):
 
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8080, workers=2, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host='127.0.0.1', port=8080, workers=2, reload=True)
